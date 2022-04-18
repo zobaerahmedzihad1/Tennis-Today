2422c0e86d83857176d3c180ad916a2c8247ed76
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-18 13:39:24 +0600

    Free trail section complete.

---
+++ src/Pages/Home/FreeTrail/FreeTrail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./FreeTrail.css";
+
+const FreeTrail = () => {
+  return (
+    <>
+      <div className="free-trail">
+        <div className="free-trail-text">
+          <h2>Register For a Free Trail Lesson</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+            aperiam.
+          </p>
+        </div>
+        <div className="free-trail-btn">
+          <button>BOOK NOW</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FreeTrail;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import FreeTrail from "../FreeTrail/FreeTrail";
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
+     <div className="hey">
       <Banner />
+      <FreeTrail/>
+     </div>
     </>
   );
 };
